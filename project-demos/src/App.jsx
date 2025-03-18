33a99475df1c8fb8c9138ebb03c4dbfaba8730b6
teddyhabtamu <tewodroshabtamu29@gmail.com>
2025-03-18 16:25:44 +0300

    simple Calculator using eval method

--- project-demos/src/App.jsx
+++ project-demos/src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Counter from './components/Counter'
 import Todo from './components/Todo'
 import Meals from './components/Meals'
+import Calculator from './components/Calculator'
 
 const App = () => {
   return (
     <div>
       {/* <Counter/> */}
       {/* <Todo/> */}
-      <Meals/>
+      {/* <Meals/> */}
+      <Calculator/>
     </div>
   )
 }
