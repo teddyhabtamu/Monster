7c625dae5af008db8e4c278817955e238632b0e3
teddyhabtamu <tewodroshabtamu29@gmail.com>
2025-03-27 15:06:05 +0300

    typescript annotation

---
+++ TS-Playground/index.ts
@@ -0,0 +1,16 @@
+
+
+// String Annotation
+let myName: string = "Teddy";
+myName = "Tewodros";
+console.log(myName)
+
+// Number Annotation
+let myAge: number = 22;
+myAge = 20 + 2;
+console.log(myAge)
+
+// Boolean Annotation
+let isStudent: boolean = true;
+isStudent = !false;
+console.log(isStudent)
