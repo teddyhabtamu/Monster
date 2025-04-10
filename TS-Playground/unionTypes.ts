58fd4d4223e8069b0d54a66c3948dd4b9c457d13
teddyhabtamu <tewodroshabtamu29@gmail.com>
2025-04-10 16:50:30 +0300

    union in type aliasing

--- TS-Playground/unionTypes.ts
+++ TS-Playground/unionTypes.ts
@@ -1,5 +1,23 @@
-let myVar: string | number = "Teddy";
+let myVar: string | number = 'Teddy';
 console.log(myVar);
 
-myVar = 6
+myVar = 6;
 
+type UserInfo = {
+  firstName: string;
+  lastName: string;
+  age: number;
+};
+
+type AccountDetails = {
+  email: string;
+  password: string;
+};
+
+let user: UserInfo | AccountDetails = {
+  firstName: 'Chebude',
+  lastName: 'Barega',
+  age: 33,
+};
+
+console.log(user);
