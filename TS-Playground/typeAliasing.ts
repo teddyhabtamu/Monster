3e2e2122bba5d5b535bc41d0e227cc62ae920a1c
teddyhabtamu <tewodroshabtamu29@gmail.com>
2025-04-10 15:35:07 +0300

    optional property in TP objects

--- TS-Playground/typeAliasing.ts
+++ TS-Playground/typeAliasing.ts
@@ -1,14 +1,16 @@
+// Type Aliasing
+
 type User = {     // using the type keyword
   name: string;
   age: number;
-  location: string;
+  location?: string;    // optional property: which means we can assign or leave as undefined
 };
 
 const printUserInfo = (user: User) => {
   return `Name: ${user.name}, Age: ${user.age}, Location: ${user.location}`
 }
 
-const result = printUserInfo({name: "Teddy", age: 22, location: "Ethiopia"})
+const result = printUserInfo({name: "Teddy", age: 22})
 
 console.log(result);
 
