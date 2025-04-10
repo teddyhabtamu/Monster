fcb46a01f027bb3b1bcac49e31be0ba75dc801f0
teddyhabtamu <tewodroshabtamu29@gmail.com>
2025-04-10 10:00:20 +0300

    Array operations like push, pop, shift, unshift

--- TS-Playground/arrays.ts
+++ TS-Playground/arrays.ts
@@ -1,7 +1,37 @@
 // type one of defining array
-const nums: number[] = [1, 2, 3, 4, 5]
+const nums: number[] = [1, 2, 3, 4, 5];
 
-console.log(nums)
+console.log(nums);
 
 // second type
-const str: Array<string> = ["Teddy", "Alex", "Marta"]
+const str: Array<string> = ['Teddy', 'Alex', 'Marta'];
+
+console.log(str);
+
+//------- push operation -----------
+
+str.push('Habtamu');
+console.log(str);
+
+nums.push(6);
+console.log(nums);
+
+// ----- unshift operation ------
+// add to the beginning
+str.unshift('tedda');
+console.log(str);
+
+
+// ---------- Pop operation ---------
+
+// remove the last element
+str.pop()
+console.log(str);
+
+// -------------- unshift operation ----------
+// remove the first element
+str.shift()
+console.log(str);
+
+
+
