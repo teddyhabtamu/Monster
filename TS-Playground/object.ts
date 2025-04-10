20a7ce6cacf2c14e53a6c1fb2ea98ad02a0fdb54
teddyhabtamu <tewodroshabtamu29@gmail.com>
2025-04-10 10:43:16 +0300

    object function parameters

--- TS-Playground/object.ts
+++ TS-Playground/object.ts
@@ -13,3 +13,18 @@ const person: {
 console.log(
   `Name: ${person.firstName} ${person.lastName}, Age: ${person.age}, Ethiopian: ${person.ethiopianNationality}`
 );
+
+// ----------------------------------
+// Function parameters
+
+function printUser(): { name: string; age: number; location: string } {
+  return {
+    name: 'Teddy',
+    age: 22,
+    location: 'Ethiopia',
+  };
+}
+
+const res = printUser()
+console.log(res);
+
