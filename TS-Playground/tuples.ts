77c7ef730391af31e8dcf5259030821ddd1b816c
teddyhabtamu <tewodroshabtamu29@gmail.com>
2025-04-11 15:27:31 +0300

    Tuples in TS

---
+++ TS-Playground/tuples.ts
@@ -0,0 +1,10 @@
+// How to create Tuples
+
+// let variableName: [type1, type2] = [value1, value2]
+
+//Example
+
+let myTuple: [string, number] = ['One', 1];
+let [alphabet, numeric] = myTuple;
+
+console.log(alphabet)
